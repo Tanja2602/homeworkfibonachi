f5f6383068d1fe2c0917b60fc1c7483811db1ae6
Tanja <tanjuscha2602@mail.ru>
2017-04-04 20:23:38 +0000

    function

---
+++ js/index.js
@@ -0,0 +1,8 @@
+  function fib(n){
+  	document. write;
+	if (n==1||n==2) return 1;
+	else return fib(n-2)+fib(n-1);		
+	}
+	var userNum=+prompt('Введіть порядковий номер числа Фібоначчі');
+  alert(fib(userNum));
+
